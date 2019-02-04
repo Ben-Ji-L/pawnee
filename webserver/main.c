2dd38dd1d6f134378d265db4c898a083c37cbde6
Coline Praca <coline.praca@gmail.com>
2019-02-04 17:25:50 +0100

    création des socket

--- webserver/main.c
+++ webserver/main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <string.h>
+#include "socket.h"
 
 int main(int argc, char **argv)
 {
-	/* Arnold Robbins in the LJ of February ’95 , describing RCS */
-	if (argc > 1 && strcmp(argv [1], "-advice") == 0) {
-		printf ("Don’t Panic !\n");
-		return 42;
+
+	creer_serveur(8080);
+
+	if (listen ( socket_serveur , 10) == -1){
+		perror ("listen socket_serveur");
+		exit(1);
 	}
-		printf ("Need an advice ?\n");
+
 	return 0;
 }
