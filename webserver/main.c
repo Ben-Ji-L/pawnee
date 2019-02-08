0eb0b92844a81e9e9cdb69e4841ed3a81197b3d0
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2019-02-08 16:59:00 +0100

    Serveur tcp basique fonctionnel

--- webserver/main.c
+++ webserver/main.c
@@ -1,16 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 #include <string.h>
+#include <unistd.h>
 #include "socket.h"
 
-int main(int argc, char **argv)
+int main()
 {
+	int socket_serveur;
+	int socket_client;
+	socket_serveur = creer_serveur(8080);
+	printf("serveur cree\n");
 
-	creer_serveur(8080);
-
-	if (listen ( socket_serveur , 10) == -1){
-		perror ("listen socket_serveur");
+	if (listen(socket_serveur, 10) == -1){
+		perror("error socket_serveur");
 		exit(1);
 	}
+
+	socket_client = accept(socket_serveur, NULL, NULL);
+	if (socket_client == -1) {
+		perror("accept error");
+		exit(1);
+	}
+
+
+	sleep(1);
+	printf("Ca marche\n");
+	const char *message = "Mais, vous savez, moi je ne crois pas\n qu\'il y ait de bonne ou de mauvaise situation.\nMoi, si je devais resumer ma vie aujourd\'hui avec vous,\nje dirais que c\'est d\'abord des rencontres,\nDes gens qui m\'ont tendu la main,\npeut-etre a un moment ou je ne pouvais pas, ou j\'étais seul chez moi.\nEt c\'est assez curieux de se dire que les hasards,\nles rencontres forgent une destinee...\nParce que quand on a le gout de la chose,\nquand on a le gout de la chose bien faite,\nLe beau geste, parfois on ne trouve pas l\'interlocuteur en face,\nje dirais, le miroir qui vous aide a avancer.\nAlors ce n\'est pas mon cas, comme je le disais la,\npuisque moi au contraire, j\'ai pu ;\nEt je dis merci a la vie, je lui dis merci,\nje chante la vie, je danse la vie... Je ne suis qu\'amour!\nEt finalement, quand beaucoup de gens aujourd\'hui me disent :\n\"Mais comment fais-tu pour avoir cette humanite \?\",\nEh bien je leur reponds tres simplement,\nje leur dis que c\'est ce gout de l\'amour,\nCe gout donc qui m\'a pousse aujourd\'hui\na entreprendre une construction mecanique,\nMais demain, qui sait, peut-etre simplement\na me mettre au service de la communaute,\na faire le don, le don de soi...\n\n";
+	/* if (write(socket_client, message, strlen(message)) == -1) {
+		perror("write error");
+		exit(1);
+	} */
+
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		if (write(socket_client, message, strlen(message)) == -1) {
+			perror("write boucle for error");
+			exit(1);
+		}
+		sleep(1);
+	}
+
+
+	while(1) {
+		char message_client[512];
+		int taille = read(socket_client, message_client, 512);
+		if (taille == -1)
+		{
+			perror("read boucle error");
+			exit(1);
+		}
+		
+		if (write(socket_client, message_client, taille) == -1)
+		{
+			perror("write boucle error");
+			exit(1);
+		}
+		
+	}
+
 
 	return 0;
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -1,23 +1,25 @@
-#include <sys/net.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
 
-int creer_serveur ( int port ){
+int creer_serveur(int port){
 
-	int socket_serveur ;
-	socket_serveur = socket (AF_INET , SOCK_STREAM , 0);
+	int socket_serveur;
+	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
 	if (socket_serveur == -1){
 		/* traitement de l ’ erreur */
-		perror ("socket_serveur ");
+		perror("socket_serveur ");
 		return -1;
 	}
 
 	struct sockaddr_in saddr ;
 	saddr.sin_family = AF_INET ; /* Socket ipv4 */
-	saddr.sin_port = htons (8080); /* Port d ’é coute */
+	saddr.sin_port = htons(port); /* Port d ’é coute */
 	saddr.sin_addr.s_addr = INADDR_ANY ; /* é coute sur toutes les interfaces */
 	
-	if (bind (socket_serveur , ( struct sockaddr *)&saddr , sizeof(saddr)) == -1){
-		perror ("bind socker_serveur");/* traitement de l ’ erreur */
+	if (bind (socket_serveur, (struct sockaddr*)&saddr , sizeof(saddr)) == -1){
+		perror("bind socker_serveur");/* traitement de l ’ erreur */
 		return -1;
 	}
 
