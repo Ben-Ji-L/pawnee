871abb0d7b48e607c90a8221181417d5c1c17922
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2019-02-27 11:56:14 +0100

    utilisation de fgets et fprintf

--- webserver/main.c
+++ webserver/main.c
@@ -65,7 +65,6 @@ void initialiser_signaux(void) {
 }
 
 void child_handler(int signal) {
-	printf("signal recu %d\n", signal);
 	while (waitpid(-1, NULL, WNOHANG) > 0) {}
 }
 
@@ -78,28 +77,15 @@ void repondre_client(int socket_client) {
 	}
 
 	if (pid == 0) {
-		char* message = "Mais, vous savez, moi je ne crois pas\n qu\'il y ait de bonne ou de mauvaise situation.\nMoi, si je devais resumer ma vie aujourd\'hui avec vous,\nje dirais que c\'est d\'abord des rencontres,\nDes gens qui m\'ont tendu la main,\npeut-etre a un moment ou je ne pouvais pas, ou j\'étais seul chez moi.\nEt c\'est assez curieux de se dire que les hasards,\nles rencontres forgent une destinee...\nParce que quand on a le gout de la chose,\nquand on a le gout de la chose bien faite,\nLe beau geste, parfois on ne trouve pas l\'interlocuteur en face,\nje dirais, le miroir qui vous aide a avancer.\nAlors ce n\'est pas mon cas, comme je le disais la,\npuisque moi au contraire, j\'ai pu ;\nEt je dis merci a la vie, je lui dis merci,\nje chante la vie, je danse la vie... Je ne suis qu\'amour!\nEt finalement, quand beaucoup de gens aujourd\'hui me disent :\n\"Mais comment fais-tu pour avoir cette humanite \?\",\nEh bien je leur reponds tres simplement,\nje leur dis que c\'est ce gout de l\'amour,\nCe gout donc qui m\'a pousse aujourd\'hui\na entreprendre une construction mecanique,\nMais demain, qui sait, peut-etre simplement\na me mettre au service de la communaute,\na faire le don, le don de soi...\n\n";
+		FILE* flux;
+		char data[512];
 
-		int i;
-		for(i = 0; i < 3; i++) {
-			if(write(socket_client, message, strlen(message)) == -1) {
-				perror("write error");
-				exit(1);
-			}
-		}
+		flux = fdopen(socket_client, "w+");
 
-		while(1) {
-			char message_client[512];
-			ssize_t taille = read(socket_client, message_client, 512);
-			if (taille == -1) {
-				perror("read boucle error");
-				exit(1);
-			} else if (taille == 0) break;
-			if(write(socket_client, message_client, taille) == -1) {
-				perror("write error");
-				exit(1);
-			}
-		}
+		fgets(data, 512, flux);
+
+		fprintf(flux, "%s", data);
+		fclose(flux);
 		exit(0);
 	}
 }
