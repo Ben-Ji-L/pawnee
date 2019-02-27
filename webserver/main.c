07c0ccd6ab868189e05d9cb77bd19cf52a4a90b5
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2019-02-27 08:12:01 +0100

    le serv ignore les signaux SIGPIPE

--- webserver/main.c
+++ webserver/main.c
@@ -32,12 +32,19 @@ int main()
 	sleep(1);
 	printf("Ca marche\n");
 	const char *message = "Mais, vous savez, moi je ne crois pas\n qu\'il y ait de bonne ou de mauvaise situation.\nMoi, si je devais resumer ma vie aujourd\'hui avec vous,\nje dirais que c\'est d\'abord des rencontres,\nDes gens qui m\'ont tendu la main,\npeut-etre a un moment ou je ne pouvais pas, ou j\'étais seul chez moi.\nEt c\'est assez curieux de se dire que les hasards,\nles rencontres forgent une destinee...\nParce que quand on a le gout de la chose,\nquand on a le gout de la chose bien faite,\nLe beau geste, parfois on ne trouve pas l\'interlocuteur en face,\nje dirais, le miroir qui vous aide a avancer.\nAlors ce n\'est pas mon cas, comme je le disais la,\npuisque moi au contraire, j\'ai pu ;\nEt je dis merci a la vie, je lui dis merci,\nje chante la vie, je danse la vie... Je ne suis qu\'amour!\nEt finalement, quand beaucoup de gens aujourd\'hui me disent :\n\"Mais comment fais-tu pour avoir cette humanite \?\",\nEh bien je leur reponds tres simplement,\nje leur dis que c\'est ce gout de l\'amour,\nCe gout donc qui m\'a pousse aujourd\'hui\na entreprendre une construction mecanique,\nMais demain, qui sait, peut-etre simplement\na me mettre au service de la communaute,\na faire le don, le don de soi...\n\n";
-    write(socket_client, message, strlen(message));
-    
+    if (write(socket_client, message, strlen(message)) == -1) {
+			perror("write boucle for error");
+			exit(1);
+		}
+
 	int i;
 	for (i = 0; i < 10; i++)
     {
-        write(socket_client, message, strlen(message));
+        if (write(socket_client, message, strlen(message)) == -1) {
+			perror("write boucle for error");
+			exit(1);
+		}
+
 		sleep(1);
 	}
 
