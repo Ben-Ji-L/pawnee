1f1e4ce897b70b9c65920fcacb6fca24da8e3d0e
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2019-03-04 13:07:31 +0100

    Le serveur envoi une réponse http correcte ainsi qu'un message de bienvenue.

--- webserver/main.c
+++ webserver/main.c
@@ -14,78 +14,95 @@ void child_handler(void);
 
 int main()
 {
-	int socket_serveur;
-	int socket_client;
-	socket_serveur = creer_serveur(8080);
+    int socket_serveur;
+    int socket_client;
+    socket_serveur = creer_serveur(8080);
 
-	printf("serveur cree\n");
+    printf("serveur cree\n");
 
-	if (listen(socket_serveur, 10) == -1){
-		perror("error socket_serveur");
-		exit(1);
-	}
-
-	initialiser_signaux();
-	sleep(1);
-	printf("Ca marche\n");
-
-	while(1) {
-		socket_client = accept(socket_serveur, NULL, NULL);
-		if (socket_client == -1) {
-			perror("accept error");
-			exit(1);
-		}
-		repondre_client(socket_client);
-		close(socket_client);	
+    if (listen(socket_serveur, 10) == -1){
+        perror("error socket_serveur");
+        exit(1);
     }
-	return 0;
+
+    initialiser_signaux();
+
+    while(1) {
+        socket_client = accept(socket_serveur, NULL, NULL);
+        if (socket_client == -1) {
+            perror("accept error");
+            exit(1);
+        }
+        repondre_client(socket_client);
+        close(socket_client);
+    }
+    return 0;
 }
 
 void initialiser_signaux(void) {
 
-	// On ignore le signal SIGPIPE
+    // On ignore le signal SIGPIPE
     if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
         perror("signal error");
         exit(1);
     }
 
-   	// Traitement signal SIGCHLD
-   	struct sigaction sa;
+    // Traitement signal SIGCHLD
+    struct sigaction sa;
 
-   	sa.sa_handler = (__sighandler_t) child_handler;
-   	sigemptyset(&sa.sa_mask);
-   	sa.sa_flags = SA_RESTART;
+    sa.sa_handler = (__sighandler_t) child_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
 
-   	if (sigaction(SIGCHLD,  &sa, NULL) == -1) {
-   		perror("error sigaction");
-   		exit(1);
-   	}
+    if (sigaction(SIGCHLD,  &sa, NULL) == -1) {
+        perror("error sigaction");
+        exit(1);
+    }
 
 
 }
 
 void child_handler(void) {
-	while (waitpid(-1, NULL, WNOHANG) > 0) {}
+    while (waitpid(-1, NULL, WNOHANG) > 0) {}
 }
 
 void repondre_client(int socket_client) {
 
-	pid_t pid = fork();
-	if (pid == -1) {
-		perror("fork error");
-		exit(1);
-	}
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork error");
+        exit(1);
+    }
 
-	if (pid == 0) {
-		FILE* flux;
-		char data[512];
+    if (pid == 0) {
+        FILE* flux;
+        char data[512];
 
-		flux = fdopen(socket_client, "w+");
+        flux = fdopen(socket_client, "w+");
 
-		fgets(data, 512, flux);
+        fgets(data, 100, flux);
+        printf("%s", data);
+        if (strncmp(data, "GET / HTTP/1.1\r\n", 17) != 0) {
+            fprintf(flux, "HTTP/1.1 400 Bad Request\r\n"
+                          "Connection: close\r\n"
+                          "Content-Length: 17\r\n"
+                          "400 Bad request\r\n");
+            fclose(flux);
+            exit(1);
+        }
 
-		printf("%s", data);
-		fclose(flux);
-		exit(0);
-	}
+        do {
+            fgets(data, 512, flux);
+        } while (strncmp(data, "\r\n", 2) != 0);
+
+        char* bienvenue = "Bonjour bienvenue sur notre serveur\r\n";
+        fprintf(flux, "HTTP/1.1 200 OK\r\n"
+                      "Connection: open\r\n"
+                      "Content-Length: %d\r\n"
+                      "200 OK\r\n\r\n", (int) strlen(bienvenue));
+
+        fprintf(flux, "%s", bienvenue);
+        fclose(flux);
+        exit(0);
+    }
 }
--- webserver/socket.c
+++ webserver/socket.c
@@ -4,30 +4,31 @@
 
 int creer_serveur(int port){
 
-	int socket_serveur;
-	int optval = 1;
+    int socket_serveur;
+    int optval = 1;
 
-	socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
+    socket_serveur = socket(AF_INET, SOCK_STREAM, 0);
 
- 	if (socket_serveur == -1){
-		/* traitement de l ’ erreur */
-		perror("socket_serveur ");
-		return -1;
-	}
+    if (socket_serveur == -1){
+        /* traitement de l ’ erreur */
+        perror("socket_serveur ");
+        return -1;
+    }
 
-	struct sockaddr_in saddr ;
-	saddr.sin_family = AF_INET ; /* Socket ipv4 */
-	saddr.sin_port = htons(port); /* Port d ’é coute */
-	saddr.sin_addr.s_addr = INADDR_ANY ; /* é coute sur toutes les interfaces */
+    struct sockaddr_in saddr ;
+    saddr.sin_family = AF_INET ; /* Socket ipv4 */
+    saddr.sin_port = htons(port); /* Port d ’é coute */
+    saddr.sin_addr.s_addr = INADDR_ANY ; /* é coute sur toutes les interfaces */
 
-	if (bind (socket_serveur, (struct sockaddr*)&saddr , sizeof(saddr)) == -1){
-		perror("bind socket_serveur");/* traitement de l ’ erreur */
-		return -1;
-	}
-	if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1) {
-	    perror("option SO_REUSEADDR error");
-		return -1;
-	}
+    if (setsockopt(socket_serveur, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) == -1) {
+        perror("option SO_REUSEADDR error");
+        return -1;
+    }
 
-	return socket_serveur;
+    if (bind(socket_serveur, (struct sockaddr*)&saddr , sizeof(saddr)) == -1){
+        perror("bind socket_serveur");/* traitement de l ’ erreur */
+        return -1;
+    }
+
+    return socket_serveur;
 }
