2eca7e3b19cfcc310246e44a6776e5aadf3990c2
Benjamin Lemaire <benjamin.lemaire59850@gmail.com>
2020-10-02 16:05:50 +0200

    test: add new tests and config the temp file for the tests

--- tests/test_http.c
+++ tests/test_http.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #include "CUnit/Basic.h"
 
@@ -13,6 +14,26 @@ void test_rewrite_target(void) {
     CU_ASSERT_STRING_EQUAL(rewrite_target("/page.html"), "page.html");
 }
 
-void test_send_response(void) {
-    send_response(temp_file, 200, "OK", "", 0);
+void test_get_date_http_format(void) {
+    time_t rawtime;
+    struct tm *timeinfo;
+    char *date = malloc(100);
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(date, 40, "%a, %d %b %G %X %Z", timeinfo);
+    CU_ASSERT_PTR_NOT_NULL(date);
+    CU_ASSERT_STRING_EQUAL(get_date_http_format(), date);
+}
+
+void test_send_status(void) {
+    send_status(get_temp_file(), 200, "OK");
+
+    const size_t line_size = 300;
+    char* line = malloc(line_size);
+    while (fgets(line, line_size, get_temp_file()) != NULL)  {
+        printf("line : %s", line);
+        CU_ASSERT_STRING_EQUAL(line, "HTTP/1.1 200 OK");
+    }
+    free(line);
 }
